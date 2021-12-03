f61de17b2199a3ca28b804deafe70fcf12228e59
Evan Suma Rosenberg <suma@umn.edu>
2021-12-02 20:19:09 -0600

    working draft

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# Assignment-6
+# Assignment 6: User Interfaces in Babylon.js
+
+**Due: Thursday, December 16, 10:00pm CDT**
+
+The purpose of this assignment is to gain experience in designing and implementing graphical user interfaces using Babylon.js and WebXR .  This is an **optional bonus** assignment.  If you choose to complete this assignment, your lowest score from the previous six assignments will be dropped and replaced with this one.  If you receive a lower score on this assignment, it will not be counted against you.
+
+## Submission Information
+
+You should fill out this information before submitting your assignment.  Make sure to document the name and source of any third party assets such as 3D models, textures, or any other content used that was not solely written by you.  Include sufficient detail for the instructor or TA to easily find them, such as asset store or download links.
+
+Name: 
+
+UMN Email:
+
+Build URL:
+
+Third Party Assets:
+
+Custom GUI Instructions:
+
+## Rubric
+
+Graded out of 20 points.  
+
+1. Add at least six mesh objects to your environment. (1)
+2. Implement selection and de-selection using the laser pointer. Make sure to provide a visual indicator for the currently selected object. (1)
+3. Create a GUI plane that is attached to the controller held in the user's non-dominant hand. Make sure to position the plane so that it is comfortable for the user to view. (2)
+4. Add a [ColorPicker](https://doc.babylonjs.com/divingDeeper/gui/gui#colorpicker) to the handheld GUI. (2)
+5. The handheld GUI should remain hidden when no object is currently selected.  When the user selects an object, the GUI panel should be made visible, and the value of the ColorPicker should be set the object's current material color. (2)
+6. Use the ColorPicker to set the material color of the currently selected object. The material should be modified in real-time as the user changes the currently selected color. (2)
+7. Create your own custom spatial GUI! You are free to design any type of user interface that you want. However, the GUI should contain at least five distinct types of interactive controls. (Non-interactive controls such as a static text block do not count.) You can find a list of control types in the [Babylon documentation](https://doc.babylonjs.com/divingDeeper/gui/gui).   You can use both 2D and 3D controls, and you can split them up among one or more GUI planes as appropriate. (1 point per control type, 5 points max)
+8. Link the GUI controls to perform useful functions such as object manipulation, locomotion, or other 3D tasks. Creativity is encouraged! Again, you are free to implement any type of object manipulation that you want. However, make sure to include instructions for using your custom GUI in the documentation above. (1 point per control type, 5 points max)
+
+Make sure to document all third party assets. ***Be aware that points will be deducted for using third party assets that are not properly documented.***
+
+## Submission
+
+You will need to check out and submit the project through GitHub classroom.  The project folder should contain just the additions to the sample project that are needed to implement the project.  Do not add extra files, and do not remove the `.gitignore` file (we do not want the "node_modules" directory in your repository.)
+
+**Do not change the names** of the existing files.  The TA needs to be able to test your program as follows:
+
+1. cd into the directory and run ```npm install```
+2. start a local web server and compile by running ```npm run start``` and pointing the browser at your ```index.html```
+
+Please test that your submission meets these requirements.  For example, after you check in your final version of the assignment to GitHub, check it out again to a new directory and make sure everything builds and runs correctly.
+
+## Local Development 
+
+After checking out the project, you need to initialize by pulling the dependencies with:
+
+```
+npm install
+```
+
+After that, you can compile and run a server with:
+
+```
+npm run start
+```
+
+Under the hood, we are using the `npx` command to both build the project (with webpack) and run a local http webserver on your machine.  The included ```package.json``` file is set up to do this automatically.  You do not have to run ```tsc``` to compile the .js files from the .ts files;  ```npx``` builds them on the fly as part of running webpack.
+
+You can run the program by pointing your web browser at ```http://localhost:8080```.
+
+To connect from your Oculus Quest, you can use [ngrok](https://ngrok.com/) to redirect a public IP address to your local machine.
+
+## License
+
+Material for [CSCI 5619 Fall 2021](https://canvas.umn.edu/courses/268490) by [Evan Suma Rosenberg](https://illusioneering.umn.edu/) is licensed under a [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-nc-sa/4.0/).
+
+The intent of choosing CC BY-NC-SA 4.0 is to allow individuals and instructors at non-profit entities to use this content.  This includes not-for-profit schools (K-12 and post-secondary). For-profit entities (or people creating courses for those sites) may not use this content without permission (this includes, but is not limited to, for-profit schools and universities and commercial education sites such as Coursera, Udacity, LinkedIn Learning, and other similar sites).   
+
+## Acknowledgments
+
+This assignment was partially based upon content from the [3D User Interfaces Fall 2020](https://github.blairmacintyre.me/3dui-class-f20) course by Blair MacIntyre.
+
+
+
---
+++ src/index.ts
@@ -0,0 +1,354 @@
+/* CSCI 5619 Fall 2021
+ * Assignment 6
+ * Instructor: Evan Suma Rosenberg <suma@umn.edu>
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ */ 
+
+import { Engine, Scene, UniversalCamera, Logger } from "@babylonjs/core";
+import { Vector3, Color3, Quaternion, Axis } from "@babylonjs/core";
+import { HemisphericLight, DirectionalLight } from "@babylonjs/core";
+import { AssetsManager, AbstractMesh } from "@babylonjs/core";
+import { WebXRCamera, WebXRInputSource, WebXRControllerComponent } from "@babylonjs/core";
+
+// Side effects
+import "@babylonjs/loaders/glTF/2.0/glTFLoader";
+import "@babylonjs/core/Helpers/sceneHelpers";
+import "@babylonjs/core/Materials/standardMaterial"
+import "@babylonjs/inspector";
+
+
+class Game 
+{ 
+    private canvas: HTMLCanvasElement;
+    private engine: Engine;
+    private scene: Scene;
+
+    private xrCamera: WebXRCamera | null; 
+    private leftController: WebXRInputSource | null;
+    private rightController: WebXRInputSource | null;
+
+    private rightGrabbedObject: AbstractMesh | null;
+    private grabbableObjects: Array<AbstractMesh>;
+
+    constructor()
+    {
+        // Get the canvas element 
+        this.canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
+
+        // Generate the BABYLON 3D engine
+        this.engine = new Engine(this.canvas, true); 
+
+        // Creates a basic Babylon Scene object
+        this.scene = new Scene(this.engine);   
+
+        // Initialize the XR variables to null
+        this.xrCamera = null;
+        this.leftController = null;
+        this.rightController = null;
+
+        // Variables for grabbing objects
+        this.rightGrabbedObject = null;
+        this.grabbableObjects = [];
+    }
+
+    start() : void 
+    {
+        // Create the scene and then execute this function afterwards
+        this.createScene().then(() => {
+
+            // Register a render loop to repeatedly render the scene
+            this.engine.runRenderLoop(() => { 
+                this.update();
+                this.scene.render();
+            });
+
+            // Watch for browser/canvas resize events
+            window.addEventListener("resize", () => { 
+                this.engine.resize();
+            });
+        });
+    }
+
+    private async createScene() 
+    {
+       // This creates and positions a first-person camera (non-mesh)
+       var camera = new UniversalCamera("camera1", new Vector3(0, 1.6, 0), this.scene);
+       camera.fov = 90 * Math.PI / 180;
+
+       // This attaches the camera to the canvas
+       camera.attachControl(this.canvas, true);
+
+       // Some ambient light to illuminate the scene
+       var ambientlight = new HemisphericLight("ambient", Vector3.Up(), this.scene);
+       ambientlight.intensity = 1.0;
+       ambientlight.diffuse = new Color3(.25, .25, .25);
+
+       // Add a directional light to imitate sunlight
+       var directionalLight = new DirectionalLight("sunlight", new Vector3(-1, -2, -1), this.scene);
+       directionalLight.position = new Vector3(0, 20, 0);
+       directionalLight.intensity = 1.0;
+
+        // Creates a default skybox
+        const environment = this.scene.createDefaultEnvironment({
+            createGround: true,
+            groundSize: 100,
+            skyboxSize: 750,
+            skyboxColor: new Color3(.059, .663, .80)
+        });
+
+         // Creates the XR experience helper
+         const xrHelper = await this.scene.createDefaultXRExperienceAsync({});
+
+         // Remove default teleportation and pointer selection
+        xrHelper.teleportation.dispose();
+
+         // Assigns the web XR camera to a member variable
+         this.xrCamera = xrHelper.baseExperience.camera;
+
+         // Assigns the left and right controllers to member variables
+         xrHelper.input.onControllerAddedObservable.add((inputSource) => 
+         {
+            if(inputSource.uniqueId.endsWith("left")) 
+            {
+                this.leftController = inputSource;
+            }
+            else 
+            {
+                this.rightController = inputSource;
+            }  
+        });
+        
+         // The assets manager can be used to load multiple assets
+         var assetsManager = new AssetsManager(this.scene);
+
+         // Add your code here
+         // Create a task for each asset you want to load
+         
+         // This loads all the assets and displays a loading screen
+         assetsManager.load();
+ 
+         // This will execute when all assets are loaded
+         assetsManager.onFinish = (tasks) => {
+
+            // Add your code here
+ 
+             // Show the debug layer
+             //this.scene.debugLayer.show();
+         }; 
+         
+    }
+
+    // The main update loop will be executed once per frame before the scene is rendered
+    private update() : void
+    {
+        this.onLeftTrigger(this.leftController?.motionController?.getComponent("xr-standard-trigger"));
+        this.onLeftSqueeze(this.leftController?.motionController?.getComponent("xr-standard-squeeze"));
+        this.onLeftThumbstick(this.leftController?.motionController?.getComponent("xr-standard-thumbstick"));
+        this.onLeftX(this.leftController?.motionController?.getComponent("x-button"));
+        this.onLeftY(this.leftController?.motionController?.getComponent("y-button"));
+
+        this.onRightTrigger(this.rightController?.motionController?.getComponent("xr-standard-trigger"));
+        this.onRightSqueeze(this.rightController?.motionController?.getComponent("xr-standard-squeeze"));
+        this.onRightThumbstick(this.rightController?.motionController?.getComponent("xr-standard-thumbstick"));
+        this.onRightA(this.rightController?.motionController?.getComponent("a-button"));
+        this.onRightB(this.rightController?.motionController?.getComponent("b-button"));
+    }
+
+    private onLeftTrigger(component?: WebXRControllerComponent)
+    {  
+        if(component?.changes.pressed)
+        {
+            if(component?.pressed)
+            {
+                Logger.Log("left trigger pressed");
+            }
+            else
+            {
+                Logger.Log("left trigger released");
+            }
+        }     
+    }
+
+    private onLeftSqueeze(component?: WebXRControllerComponent)
+    {  
+        if(component?.changes.pressed)
+        {
+            if(component?.pressed)
+            {
+                Logger.Log("left squeeze pressed");
+            }
+            else
+            {
+                Logger.Log("left squeeze released");
+            }
+        }  
+    }
+
+    private onLeftX(component?: WebXRControllerComponent)
+    {  
+        if(component?.changes.pressed)
+        {
+            if(component?.pressed)
+            {
+                Logger.Log("left X pressed");
+            }
+            else
+            {
+                Logger.Log("left X released");
+            }
+        }  
+    }
+
+    private onLeftY(component?: WebXRControllerComponent)
+    {  
+        if(component?.changes.pressed)
+        {
+            if(component?.pressed)
+            {
+                Logger.Log("left Y pressed");
+            }
+            else
+            {
+                Logger.Log("left Y released");
+            }
+        }  
+    }
+
+    private onLeftThumbstick(component?: WebXRControllerComponent)
+    {   
+        if(component?.changes.pressed)
+        {
+            if(component?.pressed)
+            {
+                Logger.Log("left thumbstick pressed");
+            }
+            else
+            {
+                Logger.Log("left thumbstick released");
+            }
+        }  
+
+        if(component?.changes.axes)
+        {
+            Logger.Log("left thumbstick axes: (" + component.axes.x + "," + component.axes.y + ")");
+        }
+    }
+
+    private onRightTrigger(component?: WebXRControllerComponent)
+    {  
+        if(component?.changes.pressed)
+        {
+            if(component?.pressed)
+            {
+                Logger.Log("right trigger pressed");
+            }
+            else
+            {
+                Logger.Log("right trigger released");
+            }
+        }  
+    }
+
+    private onRightSqueeze(component?: WebXRControllerComponent)
+    {  
+        if(component?.changes.pressed)
+        {
+            if(component?.pressed)
+            {
+                Logger.Log("right squeeze pressed");
+
+                // Check if the right controller is intersecting any grabbable objects
+                for(var i = 0; i < this.grabbableObjects.length && !this.rightGrabbedObject; i++)
+                {
+                    if(this.rightController!.grip!.intersectsMesh(this.grabbableObjects[i], true))
+                    {
+                        this.rightGrabbedObject = this.grabbableObjects[i];
+                        this.rightGrabbedObject.setParent(this.rightController!.grip!);
+                    }
+                }
+            }
+            else
+            {
+                Logger.Log("right squeeze released");
+
+                // Release the current grabbed object
+                if(this.rightGrabbedObject)
+                {
+                    this.rightGrabbedObject.setParent(null);
+                    this.rightGrabbedObject = null;
+                }
+            }
+        }  
+    }
+
+    private onRightA(component?: WebXRControllerComponent)
+    {  
+        if(component?.changes.pressed)
+        {
+            if(component?.pressed)
+            {
+                Logger.Log("right A pressed");
+            }
+            else
+            {
+                Logger.Log("right A released");
+            }
+        }  
+    }
+
+    private onRightB(component?: WebXRControllerComponent)
+    {  
+        if(component?.changes.pressed)
+        {
+            if(component?.pressed)
+            {
+                Logger.Log("right B pressed");
+            }
+            else
+            {
+                Logger.Log("right B released");
+            }
+        }  
+    }
+
+    private onRightThumbstick(component?: WebXRControllerComponent)
+    {  
+        if(component?.changes.pressed)
+        {
+            if(component?.pressed)
+            {
+                Logger.Log("right thumbstick pressed");
+            }
+            else
+            {
+                Logger.Log("right thumbstick released");
+            }
+        }  
+
+        // Check to see if the controller is connected
+        if(component)
+        {
+            // Get the current camera direction
+            var directionVector = this.xrCamera!.getDirection(Axis.Z);
+
+            // Use delta time to calculate the move distance based on speed of 3 m/sec
+            var moveDistance = -component!.axes.y * (this.engine.getDeltaTime() / 1000) * 3;
+
+            // Translate the camera forward
+            this.xrCamera!.position.addInPlace(directionVector.scale(moveDistance));
+
+            // Use delta time to calculate the turn angle based on speed of 60 degrees/sec
+            var turnAngle = component!.axes.x * (this.engine.getDeltaTime() / 1000) * 60;
+
+            // Smooth turning
+            var cameraRotation = Quaternion.FromEulerAngles(0, turnAngle * Math.PI / 180, 0);
+            this.xrCamera!.rotationQuaternion.multiplyInPlace(cameraRotation);
+        }
+ 
+    }  
+}
+/******* End of the Game class ******/   
+
+// start the game
+var game = new Game();
+game.start();
